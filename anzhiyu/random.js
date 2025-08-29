e4d55732f00eac3654cd9d3593fc9a3a23b8692d
xinzai <1970884166@qq.com>
2025-08-29 21:36:04 +0800

    Site updated: 2025-08-29 21:36:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/29/软测/理论/","2025/08/29/工具分享/资源库/"];function toRandomPost(){
+var posts=["2025/08/29/工具分享/资源库/","2025/08/29/软测/理论/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
