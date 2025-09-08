532fa3e97b999bd723ed5bad89bde53dd90f7495
xinzai <1970884166@qq.com>
2025-09-08 10:52:54 +0800

    Site updated: 2025-09-08 10:52:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/02/博客搭建相关/","2025/08/31/技术积累/博客CDN解析/","2025/08/29/软测/理论/","2025/08/29/工具分享/资源库/"];function toRandomPost(){
+var posts=["2025/09/08/0.Linux基础命令/","2025/09/02/技术积累/博客搭建相关/","2025/08/31/技术积累/博客CDN解析/","2025/08/29/软测/理论/","2025/08/29/工具分享/资源库/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
