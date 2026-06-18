df2a26102215ccf7d7443ee88835869a37c1a774
xinzai-x <1970884166@qq.com>
2026-06-19 00:20:12 +0800

    Site updated: 2026-06-19 00:20:12

--- js/main.js
+++ js/main.js
@@ -534,15 +534,6 @@
   // =============================================
   const CodeStyleFix = {
     init() {
-      document.querySelectorAll('figure.highlight').forEach(block => {
-        const pre = block.querySelector('pre');
-        if (!pre) return;
-        pre.style.background = 'none';
-        pre.style.border = 'none';
-        pre.style.borderRadius = '0';
-        pre.style.margin = '0';
-        pre.style.padding = '0';
-      });
     }
   };
 
